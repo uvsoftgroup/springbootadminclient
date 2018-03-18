7b53e30812b58f4f0e362c0be1dcd884c5c309a2
riaydhislam <riaydhislam@gmail.com>
2018-03-18 13:06:38 +0100

    initial

---
+++ src/main/java/uvsoftgroup/springbootadminclient/SpringbootadminclientApplication.java
@@ -0,0 +1,13 @@
+package uvsoftgroup.springbootadminclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringbootadminclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootadminclientApplication.class, args);
+	}
+}
